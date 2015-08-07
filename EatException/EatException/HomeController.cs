100f31886d6e0b22e859fc923027b05328c4319e
Kirill Teplinskiy <tkirill128@gmail.com>
2015-08-07 19:59:48 +0500

    Add custom 500.html

---
+++ EatException/EatException/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+
+namespace EatException
+{
+    [RoutePrefix("home")]
+    public class HomeController : Controller
+    {
+        [Route]
+        public ActionResult Index()
+        {
+//            throw new InvalidOperationException("Original exception");
+//            return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
+            Server.Transfer("~/500.cshtml");
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+    }
+}
