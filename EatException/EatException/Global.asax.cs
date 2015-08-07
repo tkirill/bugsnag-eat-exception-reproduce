ef394d59349ffa3c0dc7c5c4afab85ca3cd0a787
Kirill Teplinskiy <tkirill128@gmail.com>
2015-08-07 21:13:04 +0500

    All is ready for reproduce.
    Steps to reproduce:
    
    1. Put API key in Web.config.
    2. Build project.
    3. Navigate to the root page "/".
    4. Custom 500 page will be displayed.
    5. Comment out Bugsnag action filter in Global.asax.cs.
    6. Rebuild project.
    7. Navigate to the root page "/".
    8. Custom 500 page will be displayed again.
    9. Open C:\logs\eat-exceptions.log.
    
    In the log file will be two exceptions:
    
    * One about not found error view.  This is the case then Bugsnag action filter activated.
    * One about original exception -- InvalidOperationException.

--- EatException/EatException/Global.asax.cs
+++ EatException/EatException/Global.asax.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Bugsnag.Clients;
 
 namespace EatException
 {
@@ -10,6 +12,7 @@ namespace EatException
         protected void Application_Start(object sender, EventArgs e)
         {
             RouteTable.Routes.MapMvcAttributeRoutes();
+            GlobalFilters.Filters.Add(WebMVCClient.ErrorHandler());  // comment out to get original exception
         }
 
         protected void Session_Start(object sender, EventArgs e)
@@ -27,6 +30,7 @@ namespace EatException
 
         protected void Application_Error(object sender, EventArgs e)
         {
+            File.AppendAllText(@"C:\logs\eat-exceptions.log", "\n\n=============\n\n" + Server.GetLastError());
         }
 
         protected void Session_End(object sender, EventArgs e)
--- EatException/EatException/HomeController.cs
+++ EatException/EatException/HomeController.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-using System.Net;
 using System.Web.Mvc;
 
 namespace EatException
 {
-    [RoutePrefix("home")]
+    [RoutePrefix("")]
     public class HomeController : Controller
     {
         [Route]
-        public ActionResult Index()
+        public ActionResult Exception()
         {
-//            throw new InvalidOperationException("Original exception");
-//            return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
-            Server.Transfer("~/500.cshtml");
-            return new HttpStatusCodeResult(HttpStatusCode.OK);
+            throw new InvalidOperationException("Original exception");
         }
     }
 }
